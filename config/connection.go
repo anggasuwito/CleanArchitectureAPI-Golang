c578cdaaee4f1d9406a5073eab73da9c4ae73a8f
unknown <angga.q.suwito@gmail.com>
2020-07-05 07:05:41 +0800

    [UPDATE]config with viper

--- config/connection.go
+++ config/connection.go
@@ -2,19 +2,42 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	//driver for mysql
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
 )
 
-//Connection Connection
-func Connection() *sql.DB {
-	db, _ := sql.Open("mysql", "root:angga123@tcp(localhost:3306)/enigmaschool_api")
+//GetCustomConf GetCustomConf
+func GetCustomConf(key, defVal string) string {
+	viper.SetConfigName("configfile")
+	viper.AddConfigPath("../config/")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Printf("Keyname : %v, not found, default key value : %v, has been loaded", key, defVal)
+		return defVal
+	}
+	value, ok := viper.Get(key).(string)
+	if !ok {
+		log.Fatalf("Invalid type assertion")
+	}
+	return value
+}
 
-	if err := db.Ping(); err != nil {
+//InitDB InitDB
+func InitDB() *sql.DB {
+	dbUser := GetCustomConf("DB_USER", "root")
+	dbPass := GetCustomConf("DB_PASSWORD", "password")
+	dbHost := GetCustomConf("DB_HOST", "localhost")
+	dbPort := GetCustomConf("DB_PORT", "3306")
+	schemaName := GetCustomConf("DB_SCHEMA", "dbName")
+
+	dbPath := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, schemaName)
+	dbConn, _ := sql.Open("mysql", dbPath)
+	err := dbConn.Ping()
+	if err != nil {
 		log.Fatal(err)
 	}
-
-	return db
+	return dbConn
 }
