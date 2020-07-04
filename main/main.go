457b2b2bb51c3847a52accbcfc39825396a265de
unknown <angga.q.suwito@gmail.com>
2020-07-05 05:42:00 +0800

    [UPDATE]test using git

--- main/main.go
+++ main/main.go
@@ -1,13 +1,8 @@
 package main
 
-import (
-	"gomux/config"
-	"gomux/main/master"
-)
-
 func main() {
-	db := config.Connection()
-	router := config.CreateRouter()
-	master.Init(router, db)
-	config.RunServer(router)
+	// db := config.Connection()
+	// router := config.CreateRouter()
+	// master.Init(router, db)
+	// config.RunServer(router)
 }
