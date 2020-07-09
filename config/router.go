459034371ad7cf26890fbdd5de1cd099061b03d1
unknown <angga.q.suwito@gmail.com>
2020-07-10 05:18:27 +0800

    [DELETE]remove git ignore

--- config/router.go
+++ config/router.go
@@ -16,7 +16,6 @@ func CreateRouter() *mux.Router {
 
 //RunServer RunServer
 func RunServer(router *mux.Router) {
-	aa
 	server := GetCustomConf("server", "default")
 	port := GetCustomConf("port", "default")
 	serverAndPort := fmt.Sprintf("%v:%v", server, port)
